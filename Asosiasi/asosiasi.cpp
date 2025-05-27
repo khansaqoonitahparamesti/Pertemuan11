7737fbfda0251654695168480193c1a3800af51d
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2025-05-27 19:07:22 +0700

    menampilkan pasien dan dokter yang ingin di cetak

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -38,3 +38,47 @@ class dokter{
     void tambahpasien(pasien*);
     void cetakpasien();
 };
+
+void pasien :: tambahdokter(dokter* pDokter){
+    daftar_dokter.push_back (pDokter);
+}
+
+void pasien :: cetakdokter (){
+    cout << "Daftar dokter yang menangani pasien \""<< this-> nama << "\":\n";
+    for (auto& a : daftar_dokter){
+        cout << a-> nama << "\n";    
+    }
+    cout << endl;
+
+}
+
+int main (){
+    dokter* varDokter1 = new dokter ("dr.budi");
+    dokter* varDokter2 = new dokter ("dr.tono");
+    pasien* varPasien1 = new pasien ("Andi");
+    pasien* varPasien2 = new pasien ("Lia");
+
+
+    varDokter1 -> tambahpasien (varPasien1);
+    varDokter1 -> tambahpasien (varPasien2);
+    varDokter2 -> tambahpasien (varPasien1);
+
+
+    varPasien1 -> tambahdokter (varDokter1);
+    varPasien2 -> tambahdokter (varDokter1);
+    varPasien1 -> tambahdokter (varDokter2);
+
+    varDokter1 -> cetakpasien();
+    varDokter2 -> cetakpasien();
+    varPasien1 -> cetakdokter ();
+    varPasien2 -> cetakdokter ();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+    
+
+}
