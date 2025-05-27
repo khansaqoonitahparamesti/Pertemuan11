892cb0fe24e89ed6f2262ca38c5475d1d0439036
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2025-05-27 18:48:53 +0700

    memanggil atua membuat class dokter untuk mencetak pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -5,5 +5,36 @@ using namespace std;
 class dokter;
 
 class pasien {
-    
-}
+    public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama): nama(pNama){
+        cout << "Pasien \"" << nama << "\"ada\n";
+
+    }
+
+    ~pasien () {
+        cout << "pasien \"" << nama << "\"tidak ada\n";
+
+    }
+    void tambahdokter(dokter*);
+    void cetakdokter();    
+
+};
+
+
+class dokter{
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama): nama(pNama){
+        cout << "Dokter \"" << nama<< "\"ada\n";
+    }
+
+    ~dokter (){
+        cout << "dokter \"" << nama << "\" tidak ada \n";
+    }
+    void tambahpasien(pasien*);
+    void cetakpasien();
+};
