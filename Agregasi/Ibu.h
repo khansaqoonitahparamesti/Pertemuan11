e2882d6af2f176d297b3d10957fb17aa0eb4179d
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2025-05-27 18:23:07 +0700

    membuat klass ibu dan fungsi untuk memanggil

---
+++ Agregasi/Ibu.h
@@ -0,0 +1,33 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+    public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu (string pNama): nama (pNama){
+        cout << "ibu\""<< nama << "\"ada\n";
+    }
+
+    ~ibu ( ){
+        cout << "ibu\""<< nama << "\"tidak ada\n";
+    }
+    void tambah_Anak (anak*);
+    void cetakAnak();
+};
+
+void ibu :: tambah_Anak (anak* pAnak){
+    daftar_anak.push_back(pAnak);
+}
+
+
+void ibu :: cetakAnak (){
+    cout <<"Daftar Anak dari ibu \""<< this-> nama<< "\":\n";
+    for(int i= 0; i <daftar_anak.size(); i++){
+        cout << daftar_anak[i]->nama << endl;
+    } 
+    cout << endl;
+}
+
+#endif
